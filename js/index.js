5bf133bb082d871750963ae1286dded425e9bc30
unknown <elifgle07@gmail.com>
2022-11-14 17:57:26 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        margin: 20,
+        nav: true,
+        dots: true,
+        autoplay:true,
+        responsive: {
+            0: {
+                items: 1,
+                nav:false
+            },
+
+            990: {
+                items: 2
+            },
+            1100: {
+                items: 3
+            }
+        }
+    })
+});
+
+a = $(".stretcher-item");
+a.on({
+    mouseenter: function () {
+        $(this).addClass("active");
+        $(this).siblings().addClass("inactive")
+    },
+    mouseleave: function () {
+        $(this).removeClass("active");
+        $(this).siblings().removeClass("inactive")
+    }
+});
+$(".cards figure").on({
+    mouseenter: function () {
+        $(this).addClass("active")
+    },
+    mouseleave: function () {
+        $(this).removeClass("active")
+    }
+});
+
+
+$(document).ready(function () {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        autoPlay:true,
+        dots:true,
+        slidesToScroll:2,
+        prevArrow: "<img class='a-left control-c prev slick-prev' src='img/arrow-left.svg'>",
+        nextArrow: "<img class='a-right control-c next slick-next' src='img/arrow-right.svg'>",
+         responsive: [
+
+            {
+                breakpoint: 811,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
